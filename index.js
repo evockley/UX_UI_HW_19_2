53f94b65628124f49c7615377281871a75d4c442
evockley <77511461+evockley@users.noreply.github.com>
2021-06-22 14:05:36 -0400

    6/22/21
    functional JS in header across all pages

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
 $(".toggle").on("click", function(){
-    $(".nav-item").css("display", "flex")
+    $(".nav-item").css("display", "flex");
+    $(".close").css("display", "flex");
+    $(this).css("display", "none");
+});
+
+$(".close").on("click", function(){
+    $(".nav-item").css("display", "none");
+    $(".toggle").css("display", "flex");
+    $(this).css("display", "none");
 });
